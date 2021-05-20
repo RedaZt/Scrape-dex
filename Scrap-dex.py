db758b72b8a227df0dddc5bbe97655d74f595424
RedaZt <78495076+RedaZt@users.noreply.github.com>
2021-05-20 01:46:10 +0100

    Update Scrap-dex.py

--- Scrap-dex.py
+++ Scrap-dex.py
@@ -1,57 +1,39 @@
-import sys, os, requests, json, zipfile, urllib3
-from time import sleep
+import os, shutil, urllib3, json, zipfile, pprint
 
+dictionary = {"+" : "", "!" : "", "?" : "", "%" : "", "*" : "", "/" : "", "#" : "", "\\": "", "&" : "and", ":" : "-",  '"' : ""}
 http = urllib3.PoolManager()
 
-dictio = { "+" : "", "!" : "", "?" : "", "%" : "", "*" : "", "/" : "", "#" : "", "#" : "", "\\": "", "&" : "and", ":" : "-", }
+def zipping(directory) :
+    imgs = os.listdir(directory)
+    with zipfile.ZipFile(directory+'.cbz', 'w') as targetfile :
+        for img in imgs :
+            targetfile.write(directory+'/'+img, img)
 
-def zipping(manga_title, chapter_folder) :
-    os.chdir("C:/Users/moroc/Documents/Projects/Python/downloads/"+manga_title)
-    imgs = os.listdir(chapter_folder)
-    with zipfile.ZipFile(chapter_folder+'.cbz', 'w') as targetfile :
-        os.chdir(chapter_folder)
-        for index, file in enumerate(imgs) :
-            targetfile.write(file)
-    os.chdir("C:/Users/moroc/Documents/Projects/Python/downloads/"+manga_title)
+def titleDownloader(id) :
+    link = f"https://api.mangadex.org/manga/{id}"
+    res = http.request('GET', link)
+    data = json.loads(res.data.decode('utf8'))
 
-    
-def status_bar(total,current):
-    width_of_bar = 50
-    x = int((current / total)  * width_of_bar)
-    printed_string = "".join([('█' if i < x else ' ') for i in range(width_of_bar)])
-    out = "status : [" + printed_string + "] " + str(int((current / total) * 100)) + "%\r"
-    sys.stdout.write(out)
-    sys.stdout.flush()
+    manga_title = data["data"]["attributes"]["title"]["en"].translate(str.maketrans(dictionary))
 
-    
-def downloader(manga_id):
-    #getting manga info
-    link = "https://mangadex.org/api/manga/"+manga_id
-    R = requests.get(link)
-    data = json.loads(R.text)
-    manga_title = data['manga']['title']
-    chapters_ids = data["chapter"]
-    
-    if not os.path.exists("downloads"):
-        os.makedirs("downloads")
-    os.chdir("downloads")
+    feed_link = link = f"https://api.mangadex.org/manga/{id}/feed"
+    res2 = http.request('GET', feed_link)
+    data = json.loads(res2.data.decode('utf8'))
 
-    #finding and printing available chapters
     available_chapters = []
-    for id in chapters_ids:
-        if chapters_ids[id]["lang_code"] == "gb" :
-            if chapters_ids[id]["chapter"] == '':
-                available_chapters.append(0)    
-            else : 
-                available_chapters.append(chapters_ids[id]["chapter"])  
-    available_chapters.reverse() 
-    print(available_chapters)
+    for x in data["results"] :
+        if x["data"]["attributes"]["translatedLanguage"] == "en" : 
+            available_chapters.append(x["data"]["attributes"]["chapter"])
+    print(f"Manga : {manga_title}")
+    print("Available Chapter : ")
+    print(sorted(map(int,available_chapters)))
 
-    #prepare the list for the requested chapters
     requested_chapters = input("Chapters you want to download : ")
     while requested_chapters == '' :
         requested_chapters = input("Chapters you want to download : ")
-    if '-' in requested_chapters:
+    if requested_chapters == '*':
+        requested_chapters = available_chapters
+    elif '-' in requested_chapters:
         requested_chapters = requested_chapters.split('-')
         l = []
         for x in available_chapters:
@@ -61,71 +43,92 @@ def downloader(manga_id):
     else :
         requested_chapters = requested_chapters.replace(' ','')
         requested_chapters = requested_chapters.split(',')
-
-    chapters_to_download = []
-    for x in requested_chapters:
-        for id in chapters_ids:
-            if chapters_ids[id]["lang_code"] == "gb" and  chapters_ids[id]["chapter"] == str(x):
-                chapters_to_download.append(id)
     
-    if not os.path.exists(manga_title):
-        os.makedirs(manga_title)
-    os.chdir(manga_title)
+    print(requested_chapters)
 
-    #downloading
-    for x in chapters_to_download :
-        #getting chapter's infos
-        chapters_title = chapters_ids[x]["title"].translate(str.maketrans(dictio))
-        chapters_number = chapters_ids[x]["chapter"]
-        group_name = chapters_ids[x]["group_name"]
-        chapters_folder = manga_title+" Ch. "+chapters_number+" - "+chapters_title+" ["+group_name+']'
-        
-        #preparing chapter's folder
-        if os.path.exists(chapters_folder):
-                print(chapters_folder,"is already downloaded")
-                break
-        else:
-                os.makedirs(chapters_folder)
-        os.chdir(chapters_folder)  
 
-        #chapter's link
-        ch_link = "https://mangadex.org/api/chapter/"+x 
+def mangaSearch(title) :
+    title = '%'.join(title.split())
+    link = f"https://api.mangadex.org/manga?title={title}"
+    res = http.request('GET', link)
+    data = json.loads(res.data.decode('utf8'))
+    
+    results = {}
+    for x in data["results"] : 
+        results[x["data"]["attributes"]["title"]["en"]] = x["data"]["id"]
 
-        # downloading the pages
-        print("downloading",manga_title,"Chapter",chapters_ids[x]["chapter"])
-        r = requests.get(ch_link)
-        ch_data = json.loads(r.text)
-        server = ch_data["server"]
-        hash = ch_data["hash"]
-        images = ch_data["page_array"]
-        total = len(images)
-        downloaded = 0
-        for image in images:
-            page_link = server+hash+'/'+image
-            if os.path.exists(image):
-                os.remove(image)
-            with open(image, 'wb') as img :                
-                dt = http.request('GET', page_link)
-                img.write(dt.data)
-            downloaded += 1
-            status_bar(total,downloaded)
-            sleep(1)
-        print("\ndownloaded",manga_title,"Chapter",chapters_ids[x]["chapter"])
-        
-        zipping(manga_title, chapters_folder)
+    results_keys = list(results.keys())
+
+    for i,x in enumerate(results_keys) :
+        print(f"{i+1}. {x}")
+    
+    manga_index = int(input("Enter the number of the manga you want to download : "))
+    
+    titleDownloader(results[results_keys[manga_index-1]])
 
 
 if __name__ == "__main__" :
-    manga_url = input("link : ")
-    manga_id = ''
-    while "mangadex" and "title" not in manga_url :
-        manga_url = input("Enter a valid link : ")
-    segments = manga_url.split('/')
-    for x in segments:
-        if x.isnumeric() and len(x)>1 :
-            manga_id = x
-    if manga_id != '': 
-        downloader(manga_id)
-        print("All Done !!")
-    else :
-        print("Could not find title's id.")
+
+    id="85a99758-de39-471e-9f6d-800547f53d0a"
+    # id="7fd01936-334d-41a9-bf3e-2388aa7e514f"
+    # link = f"https://api.mangadex.org/manga/8ae4d0f6-152a-4956-8664-4405758d26ae"
+    # link = f"https://api.mangadex.org/chapter?manga=8ae4d0f6-152a-4956-8664-4405758d26ae"
+    # link = f"https://api.mangadex.org/chapter/2fdd80e9-b64e-4022-8851-8fed1c3621d0"
+    # link = "https://api.mangadex.org/at-home/server/2fdd80e9-b64e-4022-8851-8fed1c3621d0"
+    # link = "https://api.mangadex.org/manga?title="
+    # link = f"https://api.mangadex.org/manga/{id}"
+    # https://{md@h server node}/data/{data.attributes.hash}/{data.attributes.data}
+
+    title = input()
+    mangaSearch(title)
+    exit()
+    res = http.request('GET', link)
+    data = json.loads(res.data.decode('utf8'))
+
+    pprint.pprint(data)
+    exit()
+    manga_title = data["data"]["attributes"]["title"]["en"].translate(str.maketrans(dictionary))
+    directory = f"Manga/{manga_title}"
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+    link = f"https://api.mangadex.org/manga/{id}/feed"
+    res2 = http.request('GET', link)
+    data = json.loads(res2.data.decode('utf8'))
+
+    for x in data["results"] :
+        if x["data"]["attributes"]["translatedLanguage"] == "en" : 
+            pages = x["data"]["attributes"]["data"]
+            id = x["data"]["id"]
+            title = x["data"]["attributes"]["title"].translate(str.maketrans(dictionary))
+            hash = x["data"]["attributes"]["hash"]
+            chapter = x["data"]["attributes"]["chapter"]
+            groups = []
+
+            for y in x["relationships"]:
+                if y["type"] == "scanlation_group" :
+                    group_id = y["id"]
+                    group_link = f"https://api.mangadex.org/group/{group_id}"
+                    res4 = http.request('GET', group_link)
+                    group_data = json.loads(res4.data.decode('utf8'))
+                    groups.append(group_data["data"]["attributes"]["name"])
+
+            groups = ' & '.join(groups)
+
+            directory = f"Manga/{manga_title}/{manga_title} Ch. {chapter} - {title} (en) [{groups}]"
+            if not os.path.exists(directory):
+                os.makedirs(directory)
+
+            for i,page in enumerate(pages) : 
+                request_link = f"https://api.mangadex.org/at-home/server/{id}"
+                res3 = http.request('GET', request_link)
+                server = json.loads(res3.data.decode('utf8'))["baseUrl"]
+                base_link = f"{server}/data/{hash}/{page}"
+                # print(base_link)
+                ext = page.split('.')[-1]
+                with open(f"{directory}/page {str(i).zfill(3)}.{ext}", 'wb+') as img :                
+                    dt = http.request('GET', base_link)
+                    img.write(dt.data)
+            zipping(directory)
+            shutil.rmtree(directory)
+    # pprint.pprint(data)
